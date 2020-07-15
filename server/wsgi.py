3af272189656c2d7b667e2181ba79df902661881
August <August@Augusts-MacBook-Pro.local>
2020-07-15 23:40:08 +0800

    u

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,13 +1,14 @@
-import sys 
-from tornado.wsgi import WSGIContainer 
-from tornado.httpserver import HTTPServer 
-from tornado.ioloop import IOLoop 
+import sys
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from app import app  # 这里导入的是flsk项目的运行模块
 import cron
 
 port = 9000
-http_server = HTTPServer(WSGIContainer(app)) 
-http_server.listen(port) 
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(port)
+http_server.start(3)
 
 cron.start()
 IOLoop.instance().start()
