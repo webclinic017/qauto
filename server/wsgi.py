e857a07a1fb0f5d31f389de4946f8a7b4ee8f0cc
August <August@Augusts-MacBook-Pro.local>
2020-07-16 11:02:39 +0800

    u

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
@@ -12,8 +13,19 @@ import cron
 
 settings = {'debug': True}
 
-tornado.options.parse_command_line()
 
+class LogFormatter(tornado.log.LogFormatter):
+    def __init__(self):
+        super(LogFormatter, self).__init__(
+            fmt='%(color)s[%(asctime)s %(filename)s:%(funcName)s:%(lineno)d %(levelname)s]%(end_color)s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+
+
+# tornado.options.define("log_file_prefix", default="./log/app.log")
+
+tornado.options.parse_command_line()
+[i.setFormatter(LogFormatter()) for i in logging.getLogger().handlers]
 port = 9000
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(port)
